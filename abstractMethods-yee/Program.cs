b86856eaa808a0a12f60c38ca1fde4f32db0179f
trashyboyx <martinsmarcos6@icloud.com>
2020-01-31 16:32:00 -0300

    first commit

---
+++ abstractMethods-yee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace abstractMethods_yee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
